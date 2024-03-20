c1bd3a145fdf72384e05eacb3d2a8424d3b5afce
Ellery Hammond <elleryclairehammond@gmail.com>
2024-03-20 10:49:31 -0700

    header switch to logged in state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,9 @@
 import "./Header.css";
 import logo from "../../images/wtwrlogo.svg";
-// import avatar from "../../images/avatar.svg";
 import ToggleSwitch from "../ToggleSwith/ToggleSwitch";
 import { Link } from "react-router-dom";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import { useContext } from "react";
-import UserPlaceHolder from "../UserPlaceHolder/UserPlaceHolder";
 
 const Header = ({ onCreateModal, city, onSignUp, onLogin, isLoggedIn }) => {
   const currentDate = new Date().toLocaleString("default", {
@@ -13,7 +11,7 @@ const Header = ({ onCreateModal, city, onSignUp, onLogin, isLoggedIn }) => {
     day: "numeric",
   });
 
-  const { currentUser } = useContext(CurrentUserContext);
+  const currentUser = useContext(CurrentUserContext);
 
   return (
     <header className="header">
@@ -30,8 +28,8 @@ const Header = ({ onCreateModal, city, onSignUp, onLogin, isLoggedIn }) => {
 
       <div className="header__container">
         <ToggleSwitch />
-        <div>
-          {isLoggedIn ? (
+        {isLoggedIn ? (
+          <div>
             <button
               className="header__clothes-button"
               type="button"
@@ -39,42 +37,113 @@ const Header = ({ onCreateModal, city, onSignUp, onLogin, isLoggedIn }) => {
             >
               + Add clothes
             </button>
-          ) : (
-            <></>
-          )}
-        </div>
-        {isLoggedIn ? (
-          <p to="/profile" className="header__username">
-            {currentUser.name}
-          </p>
+          </div>
         ) : (
-          <Link to="/register" onClick={onSignUp} className="header__signin">
+          <button className="header__button" type="button" onClick={onSignUp}>
             Sign Up
-          </Link>
+          </button>
         )}
-
-        <div>
-          {isLoggedIn ? (
-            currentUser.avatar ? (
-              <Link to="/profile">
-                <img
-                  src={currentUser.avatar}
-                  className="header__avatar"
-                  alt="avatar"
-                />
-              </Link>
+        {isLoggedIn ? (
+          <Link to="/profile">
+            <h2 className="header__username">{currentUser?.name}</h2>
+            {currentUser?.avatar === "" ? (
+              <div className="header__user-img-text">
+                {currentUser?.name[0]}
+              </div>
             ) : (
-              <UserPlaceHolder isLoggedIn={isLoggedIn} />
-            )
-          ) : (
-            <Link to="/login" onClick={onLogin} className="header__signin">
-              Log in
-            </Link>
-          )}
-        </div>
+              <div className="header__avatar">
+                <img
+                  src={currentUser?.avatar}
+                  alt="avatar logo"
+                  className="header__user-image"
+                />
+              </div>
+            )}
+          </Link>
+        ) : (
+          <button className="header__button" type="text" onClick={onLogin}>
+            Log In
+          </button>
+        )}
       </div>
     </header>
   );
 };
 
 export default Header;
+
+// import "./Header.css";
+// import logo from "../../images/wtwrlogo.svg";
+// import ToggleSwitch from "../ToggleSwith/ToggleSwitch";
+// import { Link } from "react-router-dom";
+// import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+// import { useContext } from "react";
+
+// const Header = ({ onCreateModal, city, onSignUp, onLogin, isLoggedIn }) => {
+//   const currentDate = new Date().toLocaleString("default", {
+//     month: "long",
+//     day: "numeric",
+//   });
+
+//   const currentUser = useContext(CurrentUserContext);
+//   const avatarImage = currentUser && currentUser.avatar !== "" ? true : false;
+
+//   return (
+//     <header className="header">
+//       <div className="header__logo">
+//         {/* <div> */}
+//         <Link to="/">
+//           <img src={logo} alt="logo" />
+//         </Link>
+//         {/* </div> */}
+//         <div>
+//           {currentDate}, {city}
+//         </div>
+//       </div>
+
+//       <div className="header__container">
+//         <ToggleSwitch />
+//         {isLoggedIn ? (
+//           <>
+//             <div>
+//               <button
+//                 className="header__clothes-button"
+//                 type="button"
+//                 onClick={onCreateModal}
+//               >
+//                 + Add clothes
+//               </button>
+//             </div>
+//             <Link to="/profile">
+//               <div className="header__username">{currentUser?.name}</div>
+//             </Link>
+//             {avatarImage ? (
+//               <div className="header__avatar">
+//                 <img
+//                   src={currentUser?.avatar}
+//                   alt="avatar logo"
+//                   className="header__user-image"
+//                 ></img>
+//               </div>
+//             ) : (
+//               <p className="header__user-img-text">
+//                 {currentUser?.name[0].toUpperCase()}
+//               </p>
+//             )}
+//           </>
+//         ) : (
+//           <>
+//             <button className="header__button" type="button" onClick={onSignUp}>
+//               Sign Up
+//             </button>
+//             <button className="header__button" type="button" onClick={onLogin}>
+//               Log In
+//             </button>
+//           </>
+//         )}
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
