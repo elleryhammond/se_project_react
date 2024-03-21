00e618d8098d8f5e81632f3cc3416662d2b8571e
Ellery Hammond <elleryclairehammond@gmail.com>
2024-03-21 09:45:03 -0700

    add props for card like

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,9 +1,38 @@
 import "./ItemCard.css";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-const ItemCard = ({ item, onSelectCard }) => {
+const ItemCard = ({ item, onSelectCard, handleCardLike, isLoggedIn }) => {
+  const { currentUser } = useContext(CurrentUserContext);
+  const id = item._id;
+  const isLiked = item.likes.some((user) => {
+    return user.includes(currentUser?._id);
+  });
+
+  const itemLikeButtonClassName = `card__like-button ${
+    isLiked ? "card__like-button_active" : ""
+  }`;
+
+  const handleCardLikeClick = () => {
+    handleCardLike(id, isLiked);
+  };
+
   return (
     <div className="card__element">
-      <h2 className="card__name"> {item.name} </h2>
+      <div className="card__info">
+        <div className="card__container">
+          <div className="card__name"> {item.name} </div>
+        </div>
+        {isLoggedIn && (
+          <button
+            className={itemLikeButtonClassName}
+            type="button"
+            onClick={() => {
+              handleCardLikeClick(id, isLiked);
+            }}
+          ></button>
+        )}
+      </div>
       <img
         className="card__image"
         src={item.imageUrl}
